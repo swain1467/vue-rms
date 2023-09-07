8c30763723c8d762b98b0f73b8177a9d17a28157
swain1467 <swain1467@gmail.com>
2023-09-07 20:51:56 +0530

    started working on transition page

--- src/components/admin/Setup.vue
+++ src/components/admin/Setup.vue
@@ -9,6 +9,7 @@
         <button @click="tabType" :class="styleType">Type</button>
     </div>
     <div class="tab-content"><br>
+        <!-- All functions related to City -->
         <div class="row" v-if="showCity">
             <div v-if="showCityForm" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <input type="hidden" class="form-control" v-model="txtCityId" autocomplete="off"/>
@@ -58,7 +59,31 @@
                 </table>
             </div>
         </div>
+        <!-- All functions related to City -->
         <div class="row" v-if="showArea">
+            <div v-if="showAreaForm" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <input type="hidden" class="form-control" v-model="txtAreaId" autocomplete="off"/>
+                <div class="form-group">
+                    <label class="col-lg-12 col-md-12 col-sm-12 col-xs-12 control-label" for="txtAreaName">Area Name :&nbsp;<span class="text-danger">*</span></label>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <input type="text" class="form-control" v-model="txtAreaName" id="txtAreaName" placeholder="Enter City Name" autocomplete="off"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Status :&nbsp;<span class="text-danger">*</span></label>
+                    <input type="radio" value="1" v-model="txtAreaStatus"/><i class='fa fa-check' style='color:green; font-weight:bolder'></i>
+                    &ensp;&ensp;&ensp;
+                    <input type="radio" value="0" v-model="txtAreaStatus"/><i class='fa fa-times' style='color:red; font-weight:bolder'></i>
+                </div>
+                <div class="form-group">
+                    <br>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <button type="buttion" @click="closeForm" class="btn btn-warning"><i class="fa fa-times fa-lg"></i> close</button>&ensp;
+                        <button type="buttion" @click="saveAreaData" class="btn btn-primary pull-right"><i class="fa fa-save fa-lg"></i> Save</button>
+                    </div>
+                </div>
+                <br>
+            </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="form-group">
                     <label class="col-lg-12 col-md-12 col-sm-12 col-xs-12 control-label" for="selCity">Select City :&nbsp;<span class="text-danger">*</span></label>
@@ -70,6 +95,7 @@
                     </div>
                 </div>
                 <br>
+                <button @click="addArea" class="btn btn-success btn-sm"><i class="fa fa-plus"></i>&nbsp;Add</button>
                 <table class="table table-striped table-bordered">
                     <thead>
                         <tr>
@@ -88,15 +114,40 @@
                             <td v-if="data.status == 1" style="width:20%;" class="text-center"><i class='fa fa-check' style='color:green; font-weight:bolder'></i></td>
                             <td v-else style="width:20%;" class="text-center"><i class='fa fa-times' style='color:red; font-weight:bolder'></i></td>
                             <td style="width:20%;" class="text-center">
-                                <button class="btn btn-warning btn-sm action-btn" @click="updateArea(data.id, data.area_name, data.status)"><i class="fa fa-edit"></i></button>
+                                <button class="btn btn-warning btn-sm action-btn" @click="updateArea(data.id, data.city_id, data.area_name, data.status)"><i class="fa fa-edit"></i></button>
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </div>
         </div>
+        <!-- All functions related to City -->
         <div class="row" v-if="showType">
+            <div v-if="showTypeForm" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <input type="hidden" class="form-control" v-model="txtTypeId" autocomplete="off"/>
+                <div class="form-group">
+                    <label class="col-lg-12 col-md-12 col-sm-12 col-xs-12 control-label" for="txtType">Property Type :&nbsp;<span class="text-danger">*</span></label>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <input type="text" class="form-control" v-model="txtType" id="txtType" placeholder="Enter Property Type" autocomplete="off"/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label">Status :&nbsp;<span class="text-danger">*</span></label>
+                    <input type="radio" value="1" v-model="txtTypeStatus"/><i class='fa fa-check' style='color:green; font-weight:bolder'></i>
+                    &ensp;&ensp;&ensp;
+                    <input type="radio" value="0" v-model="txtTypeStatus"/><i class='fa fa-times' style='color:red; font-weight:bolder'></i>
+                </div>
+                <div class="form-group">
+                    <br>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <button type="buttion" @click="closeForm" class="btn btn-warning"><i class="fa fa-times fa-lg"></i> close</button>&ensp;
+                        <button type="buttion" @click="saveTypeData" class="btn btn-primary pull-right"><i class="fa fa-save fa-lg"></i> Save</button>
+                    </div>
+                </div>
+                <br>
+            </div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <button @click="addType" class="btn btn-success btn-sm"><i class="fa fa-plus"></i>&nbsp;Add</button>
                 <table class="table table-striped table-bordered">
                     <thead>
                         <tr>
@@ -106,7 +157,17 @@
                             <th class="text-center text-white bg-primary">Action</th>
                         </tr>
                     </thead>
-                    <tbody></tbody>
+                    <tbody>
+                        <tr v-for='data in typeList' :key='data.id'>
+                            <td style="width:10%;" class="text-center">{{data.sl_no}}</td>
+                            <td style="width:50%;" class="text-left">{{data.type}}</td>
+                            <td v-if="data.status == 1" style="width:20%;" class="text-center"><i class='fa fa-check' style='color:green; font-weight:bolder'></i></td>
+                            <td v-else style="width:20%;" class="text-center"><i class='fa fa-times' style='color:red; font-weight:bolder'></i></td>
+                            <td style="width:20%;" class="text-center">
+                                <button class="btn btn-warning btn-sm action-btn" @click="updateType(data.id, data.type, data.status)"><i class="fa fa-edit"></i></button>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
@@ -143,6 +204,16 @@ export default {
             selCity:'',
             areaCityList:'',
             areaList:'',
+            showAreaForm: false,
+            txtAreaId:'',
+            txtAreaName:'',
+            txtAreaStatus:'',
+
+            typeList: '',
+            showTypeForm: false,
+            txtTypeId: '',
+            txtType: '',
+            txtTypeStatus: '',
         }
     },
 
@@ -162,6 +233,17 @@ export default {
             res => {
                 this.cityList = res.data.aaData
                 this.areaCityList = res.data.aaData1
+            }
+        ).catch(
+            err => {
+                console.log(err);
+            }
+        );
+
+        axios.get('auth/GetHouseTypeList',config)
+        .then(
+            res => {
+                this.typeList = res.data.aaData
             }
         ).catch(
             err => {
@@ -194,6 +276,7 @@ export default {
             this.styleArea = 'btn btn-dark btn-sm'
             this.styleType = 'btn btn-danger btn-sm'
         },
+        //All functions related to City
         addCity(){
             this.showCityForm = true;
             this.txtCityId = '';
@@ -202,6 +285,8 @@ export default {
         },
         closeForm(){
             this.showCityForm = false;
+            this.showAreaForm = false;
+            this.showTypeForm = false;
         },
         updateCity(id, city_name, city_status){
             this.txtCityId = id
@@ -263,6 +348,7 @@ export default {
                     }
                 );
         },
+        //All functions related to Area
         getAreaData(){
             const config = {
                 headers: { Authorization: `Bearer ${this.token}` }
@@ -280,6 +366,141 @@ export default {
                     console.log(err);
                 }
             );
+        },
+        addArea(){
+            this.showAreaForm = true;
+            this.txtAreaId = '';
+            this.txtAreaName = '';
+            this.txtAreaStatus = 1
+        },
+        updateArea(id, city_id, area_name, area_status){
+            this.txtAreaId = id
+            this.txtAreaName = area_name
+            if(area_status == 1){
+                this.txtAreaStatus = 1
+            }else{
+                this.txtAreaStatus = 0
+            }
+            this.showAreaForm = true;
+        },
+        saveAreaData(){
+            const config = {
+                headers: { Authorization: `Bearer ${this.token}` }
+            };
+            const bodyParameters = {
+                id: this.user_id,
+                area_id: this.txtAreaId,
+                city_id: this.selCity,
+                area_name: this.txtAreaName,
+                area_status: this.txtAreaStatus,
+            };
+            axios.post( 'auth/SaveArea',bodyParameters,config)
+            .then(
+                res => {
+                    if(res.data.status == 'Success'){
+                        this.$toast.success(res.data.message, {
+                            position: 'top'
+                        })
+                        this.showAreaForm = false;
+                    }else if(res.data.status == 'Error'){
+                        this.$toast.warning(res.data.message, {
+                            position: 'top'
+                        })
+                    }else{
+                        this.$toast.warning('Sorry something went wrong', {
+                            position: 'top'
+                        })
+                    }
+                    setTimeout(this.$toast.clear, 1000)
+                        this.txtAreaId= '',
+                        this.txtAreaName= '',
+                        this.txtAreaStatus= ''
+                        
+                        const bodyParameters = {
+                        city_id: this.selCity
+                        };
+                        axios.post( 'auth/GetAreaList',bodyParameters,config)
+                        .then(
+                            res => {
+                                this.areaList = res.data.aaData
+                            }
+                        ).catch(
+                            err => {
+                                console.log(err);
+                            }
+                        );
+                    }
+                ).catch(
+                    err => {
+                        console.log(err);
+                    }
+                );
+        },
+        //All functions related to Propert Type
+        addType(){
+            this.showTypeForm = true;
+            this.txtTypeId = '';
+            this.txtType = '';
+            this.txtTypeStatus = 1
+        },
+        updateType(id, type, type_status){
+            this.txtTypeId = id
+            this.txtType = type
+            if(type_status == 1){
+                this.txtTypeStatus = 1
+            }else{
+                this.txtTypeStatus = 0
+            }
+            this.showTypeForm = true;
+        },
+        saveTypeData(){
+            const config = {
+                headers: { Authorization: `Bearer ${this.token}` }
+            };
+            const bodyParameters = {
+                id: this.user_id,
+                type_id: this.txtTypeId,
+                type: this.txtType,
+                type_status: this.txtTypeStatus,
+            };
+            axios.post( 'auth/SaveHouseType',bodyParameters,config)
+            .then(
+                res => {
+                    if(res.data.status == 'Success'){
+                        this.$toast.success(res.data.message, {
+                            position: 'top'
+                        })
+                        this.showTypeForm = false;
+                    }else if(res.data.status == 'Error'){
+                        this.$toast.warning(res.data.message, {
+                            position: 'top'
+                        })
+                    }else{
+                        this.$toast.warning('Sorry something went wrong', {
+                            position: 'top'
+                        })
+                    }
+                    setTimeout(this.$toast.clear, 1000)
+                        this.txtTypeId= '',
+                        this.txtType= '',
+                        this.txtTypeStatus= ''
+                        
+                        axios.get('auth/GetHouseTypeList',config)
+                        .then(
+                            res => {
+                                this.typeList = res.data.aaData
+                            }
+                        ).catch(
+                            err => {
+                                console.log(err);
+                            }
+                        );
+                    }
+                ).catch(
+                    err => {
+                        console.log(err);
+                    }
+                );
         },
     }
 }
--- src/components/admin/TransitionPage.vue
+++ src/components/admin/TransitionPage.vue
@@ -1,13 +1,65 @@
 <template>
 <Nav/>
 <div class="row">
-
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="row">
+            <div class="form-group">
+                <label class="col-lg-2 col-md-2 col-sm-2 col-xs-2 control-label" for="selCity">Select City :&nbsp;<span class="text-danger">*</span></label>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <select class="form-control" id="selCity" v-model="selCity" @change="getHouseData();">
+                        <option value= '' disabled>Select a city</option>
+                        <option v-for='data in cityList' :key='data.id' :value='data.id'>{{ data.city_name }}</option>
+                    </select>
+                </div>
+                 <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1"></div>
+                <label class="col-lg-1 col-md-1 col-sm-1 col-xs-1 control-label" for="search">Search :&nbsp;<span class="text-danger">*</span></label>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <input type="text" class="form-control" v-model="txtSearch" id="search" @input="getHouseData();" placeholder="Search..." autocomplete="off"/>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <br>
+				<table class="table table-striped table-bordered" id="dtblHD">
+                    <thead class="bg-primary">
+                        <tr>
+                            <th class="text-center text-white bg-primary">Sl.No</th>
+                            <th class="text-center text-white bg-primary">User's Mail</th>
+                            <th class="text-center text-white bg-primary">Contact No</th>
+                            <th class="text-center text-white bg-primary" title="Available From">Avl From</th>
+                            <th class="text-center text-white bg-primary">Type</th>
+                            <th class="text-center text-white bg-primary">Area</th>
+                            <th class="text-center text-white bg-primary">City</th>
+                            <th class="text-center text-white bg-primary">Status</th>
+                            <th class="text-center text-white bg-primary">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for='data in houseList' :key='data.id'>
+                            <td style="width:5%;" class="text-center">{{data.sl_no}}</td>
+                            <td style="width:15%;" class="text-left">{{data.user.email}}</td>
+                            <td style="width:10%;" class="text-left">{{data.contact_no}}</td>
+                            <td style="width:10%;" class="text-left">{{data.from_date}}</td>
+                            <td style="width:10%;" class="text-left">{{data.type.type}}</td>
+                            <td style="width:15%;" class="text-left">{{data.area.area_name}}</td>
+                            <td style="width:15%;" class="text-left">{{data.city.city_name}}</td>
+                            <td v-if="data.status == 1" style="width:5%;" class="text-center"><i class='fa fa-check' style='color:green; font-weight:bolder'></i></td>
+                            <td v-else style="width:5%;" class="text-center"><i class='fa fa-times' style='color:red; font-weight:bolder'></i></td>
+                            <td style="width:10%;" class="text-center">
+                                <button class="btn btn-danger btn-sm action-btn" @click="deleteHD(data.id)"><i class="fa fa-trash"></i></button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+			</div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
 import Nav from '@/components/Nav.vue'
 import isAdmin from '../../composables/isAdmin.js'
+import axios from 'axios'
 
 export default {
     components:{
@@ -15,11 +67,11 @@ export default {
     },
     data(){
         return{
-            user_name:'',
-            user_type:'',
-            user_id:'',
-
-            showEmail: true,
+           token: '',
+           cityList: '',
+           selCity: '',
+           txtSearch: '',
+           houseList: '',
         }
     },
 
@@ -27,9 +79,99 @@ export default {
         if(isAdmin() == 0){
             this.$router.push({ name: 'sign_in'})
         }
+
+        this.token = JSON.parse(localStorage.getItem('token'))
+        const config = {
+            headers: { Authorization: `Bearer ${this.token}` }
+        };
+        //City List
+        axios.get( 'auth/GetCity',config)
+        .then(
+            res => {
+                this.cityList = res.data.aaData
+            }
+        ).catch(
+            err => {
+                console.log(err);
+            }
+        );
     },
+    methods:{
+        getHouseData(){
+            const config = {
+                headers: { Authorization: `Bearer ${this.token}` }
+            };
+            const bodyParameters = {
+            city_id: this.selCity,
+            search: this.txtSearch
+            };
+            axios.post( 'auth/GetHouseDetailsList',bodyParameters,config)
+            .then(
+                res => {
+                    this.houseList = res.data.aaData
+                }
+            ).catch(
+                err => {
+                    console.log(err);
+                }
+            );
+        },
+        deleteHD(id){
+            let text = 'Are you sure to delete ?'
+            if (confirm(text) == true) {
+               
+            const config = {
+                headers: { Authorization: `Bearer ${this.token}` }
+            };
+
+            const bodyParameters = {
+                id: id,
+            };
+            axios.post( 'auth/DeleteHouse',bodyParameters,config)
+            .then(
+                res => {
+                    if(res.data.status == 'Success'){
+                        this.$toast.success(res.data.message, {
+                            position: 'top'
+                        })
+                        const bodyParameters = {
+                        city_id: this.selCity,
+                        search: this.txtSearch
+                        };
+                        axios.post( 'auth/GetHouseDetailsList',bodyParameters,config)
+                        .then(
+                            res => {
+                                this.houseList = res.data.aaData
+                            }
+                        ).catch(
+                            err => {
+                                console.log(err);
+                            }
+                        );
+                    }else if(res.data.status == 'Error'){
+                        this.$toast.warning(res.data.message, {
+                            position: 'top'
+                        })
+                    }else{
+                        this.$toast.warning('Sorry something went wrong', {
+                            position: 'top'
+                        })
+                    }
+                    setTimeout(this.$toast.clear, 1000)
+                    }
+                ).catch(
+                    err => {
+                        console.log(err);
+                    }
+                ); 
+            }
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.form-group {
+  display: inline-flex;
+}
 </style>
